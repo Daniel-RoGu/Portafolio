9a8b1109899b7375e1f74bedf4fedf51e5697651
Daniel-RoGu <danrogu98@gmail.com>
2024-02-04 15:41:35 -0500

    Commit_016

--- Proyecto Colegio/App/ProyectoColegio/Controllers/FuncionarioController.cs
+++ Proyecto Colegio/App/ProyectoColegio/Controllers/FuncionarioController.cs
@@ -1,14 +1,253 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using ProyectoColegio.Data;
 using ProyectoColegio.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Hosting;
+using CsvHelper;
+using CsvHelper.Configuration.Attributes;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic.FileIO;
+using System.Text;
+using CsvHelper.Configuration;
+using System.Globalization;
 
 namespace ProyectoColegio.Controllers
 {
     public class FuncionarioController : Controller
     {
         Funcionario funcionario = new Funcionario();
-        public IActionResult Index()
+        //InfoSimat infoCsv = new InfoSimat();
+        private readonly Contexto _contexto;
+        //private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public FuncionarioController(Contexto contexto)
         {
+            _contexto = contexto;
+        }
+
+        //public FuncionarioController(IWebHostEnvironment webHostEnvironment)
+        //{
+        //    _webHostEnvironment = webHostEnvironment;
+        //}
+
+
+        public IActionResult CargarCsv()
+        {        
             return View();
         }
+
+        [HttpPost]
+        public IActionResult CargarCsv(IFormFile file)
+        {
+
+            //string rutaArchivo = @"C:\Users\Daniel\Desktop\TrabajoColegio\Guia.csv";
+            var tempFilePath = "";
+
+            if (file != null && file.Length > 0)
+            {
+                // Crear un archivo temporal
+                tempFilePath = Path.GetTempFileName();
+
+                // Guardar el contenido del archivo en el archivo temporal
+                using (var stream = new FileStream(tempFilePath, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+
+            }
+
+            // Lista para almacenar los datos
+            List<InfoCsv> info = new List<InfoCsv>();
+
+            var csvConfig = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                Delimiter = ";",
+                HasHeaderRecord = true,
+            };
+
+            // Leer el archivo CSV usando CsvHelper
+            using (var reader = new StreamReader(tempFilePath))
+            //using (var csv = new CsvReader(reader, new CsvHelper.Configuration.CsvConfiguration(System.Globalization.CultureInfo.InvariantCulture)))
+            using (var csv = new CsvReader(reader, csvConfig))
+            {
+                // Lee todos los registros a la vez
+                var records = csv.GetRecords<InfoCsv>().ToList();
+
+                // Agrega los registros a la lista
+                info.AddRange(records);
+            }
+
+            // Eliminar el archivo temporal después de procesarlo si es necesario
+            System.IO.File.Delete(tempFilePath);
+
+            // Procesa la lista
+            UsarCsv(info);
+
+            return View();
+        }
+
+        public void UsarCsv(List<InfoCsv> datos)
+        {
+
+            try
+            {
+
+                foreach (InfoCsv dato in datos)
+                {
+                    using (MySqlConnection conexion = new MySqlConnection(_contexto.Conexion))
+                    {
+                        conexion.Open();
+                        MySqlCommand Command = new MySqlCommand("registrarTipoSangre", conexion);
+                        Command.CommandType = System.Data.CommandType.StoredProcedure;
+                        Command.Parameters.AddWithValue("nomTpSangre", dato.NOMBRE1);
+                        Command.ExecuteNonQuery();
+                    }
+                    Console.WriteLine(dato);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error al ejecutar el script: {ex.Message}");
+            }
+
+        }       
+
+        public class InfoCsv
+        {
+            [Name("ANO")]
+            public int? ANO { get; set; }
+
+            [Name("ETC")]
+            public String? ETC { get; set; }
+
+            [Name("ESTADO")]
+            public String? ESTADO { get; set; }
+
+            [Name("JERARQUIA")]
+            public String? JERARQUIA { get; set; }
+
+            [Name("INSTITUCION")]
+            public String? INSTITUCION { get; set; }
+            
+            [Name("DANE")]
+            public String? DANE { get; set; }
+
+            [Name("CALENDARIO")]
+            public String? CALENDARIO { get; set; }
+
+            [Name("SECTOR")]
+            public String? SECTOR { get; set; }
+
+            [Name("SEDE")]
+            public String? SEDE { get; set; }
+            
+            [Name("CODIGO_DANE_SEDE")]
+            public String? CODIGO_DANE_SEDE { get; set; }
+
+            [Name("CONSECUTIVO")]
+            public String? CONSECUTIVO { get; set; }
+
+            [Name("ZONA_SEDE")]
+            public String? ZONA_SEDE { get; set; }
+
+            [Name("JORNADA")]
+            public String? JORNADA { get; set; }
+
+            [Name("GRADO_COD")]
+            public int? GRADO_COD { get; set; }
+
+            [Name("GRUPO")]
+            public int? GRUPO { get; set; }
+
+            [Name("MODELO")]
+            public String? MODELO { get; set; }
+            
+            [Name("MOTIVO")]
+            public String? MOTIVO { get; set; }
+
+            [Name("FECHAINI")]
+            public String? FECHAINI { get; set; }
+
+            [Name("FECHAFIN")]
+            public String? FECHAFIN { get; set; }
+
+            [Name("NUI")]
+            public String? NUI { get; set; }
+
+            [Name("ESTRATO")]
+            public String? ESTRATO { get; set; }
+
+            [Name("SISBEN IV")]
+            public String? SISBEN_IV { get; set; }
+
+            [Name("PER_ID")]
+            public int? PER_ID { get; set; }
+
+            [Name("DOC")]
+            public string? DOC { get; set; }
+
+            [Name("TIPODOC")]
+            public String? TIPODOC { get; set; }
+
+            [Name("APELLIDO1")]
+            public String? APELLIDO1 { get; set; }
+
+            [Name("APELLIDO2")]
+            public String? APELLIDO2 { get; set; }
+
+            [Name("NOMBRE1")]
+            public String? NOMBRE1 { get; set; }
+
+            [Name("NOMBRE2")]
+            public String? NOMBRE2 { get; set; }
+
+            [Name("GENERO")]
+            public String? GENERO { get; set; }
+
+            [Name("FECHA_NACIMIENTO")]
+            public String? FECHA_NACIMIENTO { get; set; }
+
+            [Name("BARRIO")]
+            public String? BARRIO { get; set; }
+
+            [Name("EPS")]
+            public String? EPS { get; set; }
+
+            [Name("TIPO DE SANGRE")]
+            public String? TIPO_DE_SANGRE { get; set; }
+
+            [Name("MATRICULACONTRATADA")]
+            public String? MATRICULACONTRATADA { get; set; }
+
+            [Name("FUENTE_RECURSOS")]
+            public String? FUENTE_RECURSOS { get; set; }
+
+            [Name("INTERNADO")]
+            public String? INTERNADO { get; set; }
+
+            [Name("NUM_CONTRATO")]
+            public String? NUM_CONTRATO { get; set; }
+
+            [Name("APOYO_ACADEMICO_ESPECIAL")]
+            public String? APOYO_ACADEMICO_ESPECIAL { get; set; }
+
+            [Name("SRPA")]
+            public String? SRPA { get; set; }
+
+            [Name("DISCAPACIDAD")]
+            public String? DISCAPACIDAD { get; set; }
+
+            [Name("PAIS_ORIGEN")]
+            public String? PAIS_ORIGEN { get; set; }
+
+            [Name("CORREO")]
+            public String? CORREO { get; set; }
+        }
+
     }
 }
---
+++ Proyecto Colegio/App/ProyectoColegio/Models/InfoSimat.cs
@@ -0,0 +1,324 @@
+﻿using CsvHelper.Configuration.Attributes;
+
+namespace ProyectoColegio.Models
+{
+    public class InfoSimat
+    {
+        [Name("ANO")]
+        private int? _annoElectivo;
+
+        [Name("ETC")]
+        private String? _departamento;
+
+        [Name("ESTADO")]
+        private String? _estadoMatricula;
+
+        [Name("JERARQUIA")]
+        private String? _municipio;
+
+        [Name("INSTITUCION")]
+        private String? _institucion;
+
+        [Name("CALENDARIO")]
+        private String? _calendario;
+
+        [Name("SECTOR")]
+        private String? _sector;
+
+        [Name("SEDE")]
+        private String? _sede;
+
+        [Name("ZONA_SEDE")]
+        private String? _zonaSede;
+
+        [Name("JORNADA")]
+        private String? _jornada;
+
+        [Name("GRADO_COD")]
+        private int? _grado;
+
+        [Name("GRUPO")]
+        private int? _grupo;
+
+        [Name("MODELO")]
+        private String? _modalidad;
+
+        [Name("FECHAINI")]
+        private String? _fechaInicio;
+
+        [Name("FECHAFIN")]
+        private String? _fechaFin;
+
+        [Name("NUI")]
+        private String? _NUI;
+
+        [Name("ESTRATO")]
+        private String? _estrato;
+
+        [Name("SISBEN IV")]
+        private String? _sisben;
+
+        [Name("PER_ID")]
+        private int? _PERID;
+
+        [Name("DOC")]
+        private int? _numeroIdentificacion;
+
+        [Name("TIPODOC")]
+        private String? _tipoIdentificacion;
+
+        [Name("APELLIDO1")]
+        private String? _apellido1;
+
+        [Name("APELLIDO2")]
+        private String? _apellido2;
+
+        [Name("NOMBRE1")]
+        private String? _nombre1;
+
+        [Name("NOMBRE2")]
+        private String? _nombre2;
+
+        [Name("GENERO")]
+        private String? _genero;
+
+        [Name("FECHA_NACIMIENTO")]
+        private String? _fechaNacimiento;
+
+        [Name("BARRIO")]
+        private String? _barrio;
+
+        [Name("EPS")]
+        private String? _eps;
+
+        [Name("TIPO DE SANGRE")]
+        private String? _tipoSangre;
+
+        [Name("INTERNADO")]
+        private String? _internado;
+
+        [Name("APOYO_ACADEMICO_ESPECIAL")]
+        private String? _apoyoAcademicoEspecial;
+
+        [Name("DISCAPACIDAD")]
+        private String? _discapacidad;
+
+        [Name("PAIS_ORIGEN")]
+        private String? _paisOrigen;
+
+        [Name("CORREO")]
+        private String? _correo;
+
+        public int? AnnoElectivo
+        {
+            get { return _annoElectivo; }
+            set { _annoElectivo = value; }
+        }
+
+        public string? Departamento
+        {
+            get { return _departamento; }
+            set { _departamento = value; }
+        }
+
+        public string? EstadoMatricula
+        {
+            get { return _estadoMatricula; }
+            set { _estadoMatricula = value; }
+        }
+
+        public string? Municipio
+        {
+            get { return _municipio; }
+            set { _municipio = value; }
+        }
+
+        public string? Institucion
+        {
+            get { return _institucion; }
+            set { _institucion = value; }
+        }
+
+        public string? Calendario
+        {
+            get { return _calendario; }
+            set { _calendario = value; }
+        }
+
+        public string? Sector
+        {
+            get { return _sector; }
+            set { _sector = value; }
+        }
+
+        public string? Sede
+        {
+            get { return _sede; }
+            set { _sede = value; }
+        }
+
+        public string? ZonaSede
+        {
+            get { return _zonaSede; }
+            set { _zonaSede = value; }
+        }
+
+        public string? Jornada
+        {
+            get { return _jornada; }
+            set { _jornada = value; }
+        }
+
+        public int? Grado
+        {
+            get { return _grado; }
+            set { _grado = value; }
+        }
+
+        public int? Grupo
+        {
+            get { return _grupo; }
+            set { _grupo = value; }
+        }
+
+        public string? Modalidad
+        {
+            get { return _modalidad; }
+            set { _modalidad = value; }
+        }
+
+        public string? FechaInicio
+        {
+            get { return _fechaInicio; }
+            set { _fechaInicio = value; }
+        }
+
+        public string? FechaFin
+        {
+            get { return _fechaFin; }
+            set { _fechaFin = value; }
+        }
+
+        public string? NUI
+        {
+            get { return _NUI; }
+            set { _NUI = value; }
+        }
+
+        public string? Estrato
+        {
+            get { return _estrato; }
+            set { _estrato = value; }
+        }
+
+        public string? Sisben
+        {
+            get { return _sisben; }
+            set { _sisben = value; }
+        }
+
+        public int? PERID
+        {
+            get { return _PERID; }
+            set { _PERID = value; }
+        }
+
+        public int? NumeroIdentificacion
+        {
+            get { return _numeroIdentificacion; }
+            set { _numeroIdentificacion = value; }
+        }
+
+        public string? TipoIdentificacion
+        {
+            get { return _tipoIdentificacion; }
+            set { _tipoIdentificacion = value; }
+        }
+
+        public string? Apellido1
+        {
+            get { return _apellido1; }
+            set { _apellido1 = value; }
+        }
+
+        public string? Apellido2
+        {
+            get { return _apellido2; }
+            set { _apellido2 = value; }
+        }
+
+        public string? Nombre1
+        {
+            get { return _nombre1; }
+            set { _nombre1 = value; }
+        }
+
+        public string? Nombre2
+        {
+            get { return _nombre2; }
+            set { _nombre2 = value; }
+        }
+
+        public string? Genero
+        {
+            get { return _genero; }
+            set { _genero = value; }
+        }
+
+        public string? FechaNacimiento
+        {
+            get { return _fechaNacimiento; }
+            set { _fechaNacimiento = value; }
+        }
+
+        public string? Barrio
+        {
+            get { return _barrio; }
+            set { _barrio = value; }
+        }
+
+        public string? Eps
+        {
+            get { return _eps; }
+            set { _eps = value; }
+        }
+
+        public string? TipoSangre
+        {
+            get { return _tipoSangre; }
+            set { _tipoSangre = value; }
+        }
+
+        public string? Internado
+        {
+            get { return _internado; }
+            set { _internado = value; }
+        }
+
+        public string? ApoyoAcademicoEspecial
+        {
+            get { return _apoyoAcademicoEspecial; }
+            set { _apoyoAcademicoEspecial = value; }
+        }
+
+        public string? Discapacidad
+        {
+            get { return _discapacidad; }
+            set { _discapacidad = value; }
+        }
+
+        public string? PaisOrigen
+        {
+            get { return _paisOrigen; }
+            set { _paisOrigen = value; }
+        }
+
+        public string? Correo
+        {
+            get { return _correo; }
+            set { _correo = value; }
+        }
+
+    }
+ }
+ 
